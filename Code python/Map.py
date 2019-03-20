fbd98702dcaf5ccdd3654d570b185494606888a1
clement <clement413@gmail.com>
2019-03-20 18:31:09 +0100

    add Map code

---
+++ Code python/Map.py
@@ -0,0 +1,14 @@
+class Map:
+    def __init__(self, tailleX, tailleY):
+        self.__tailleX = tailleX
+        self.__tailleY = tailleY
+
+    def genererMap(self):
+        print("générer map")
+
+    def recupererSalle(self, x, y):
+        return 0
+
+    def seDeplacer(self):
+        print("je me déplace")
+
