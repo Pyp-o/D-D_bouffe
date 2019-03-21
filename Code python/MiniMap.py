82ee6943891ce51242f3c3487f74c7c758d468e7
clement <clement413@gmail.com>
2019-03-21 19:19:15 +0100

    add MiniMap code

---
+++ Code python/MiniMap.py
@@ -0,0 +1,9 @@
+class MiniMap:
+    def __init__(self, map):
+        self.__map = map
+
+    def decouvrir(self, x, y):
+        print("ce lieu est découvert")
+
+    def afficher(self):
+        print("j'affiche la carte")
