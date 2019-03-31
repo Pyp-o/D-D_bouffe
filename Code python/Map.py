c5d7e630d63bc5dfa88f5e3a79ff5a081ad46d81
clement <clement413@gmail.com>
2019-03-31 18:45:10 +0200

    map fini

--- Code python/Map.py
+++ Code python/Map.py
@@ -1,10 +1,13 @@
 from random import choice, randrange as rand
 from Salle import *
+import sys
 
 class Map:
     def __init__(self, tailleX, tailleY):
         self.__tailleX = tailleX
         self.__tailleY = tailleY
+        self.__positionX = 0
+        self.__positionY = 0
 
     def genererMap(self):
 
@@ -24,6 +27,7 @@ class Map:
         ##
         # Structures de données
         self.__grid = [([0] * self.width) for _ in range(self.height)]
+        self.__salles = [([0] * self.width) for _ in range(self.height)]
         self.frontier = set()
         ##
         # Exécution du script
@@ -33,6 +37,9 @@ class Map:
             if len(argv) == 3:
                 self.width, self.height = map(int, argv[1:])
                 self.__grid = [[0] * self.width for _ in range(self.height)]
+                self.__salles = [([0] * self.width) for _ in range(self.height)]
+
+            self.creerSalle()
 
             self.mark(rand(self.width), rand(self.height))
             while self.frontier:
@@ -51,15 +58,24 @@ class Map:
                 self.mark(x, y)
 
                 ## DEBUG :
-                # print("frontier =", frontier)
-                # print("(%d, %d) -> (%d, %d)" % (x, y, nx, ny))
-                # display_maze()
-                # input("Appuyez sur Entree pour continuer")
+                #print("frontier =", self.frontier)
+                #print("(%d, %d) -> (%d, %d)" % (x, y, nx, ny))
+                #self.display_maze()
+                #input("Appuyez sur Entree pour continuer")
 
-            self.display_maze()
-        print("générer map")
+            #self.display_maze()
+        #print("générer map")
+        self.creationFrontiereSalle()
+
         ##
         # Méthodes utilitaires
+
+    def creerSalle(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                self.__salles[x][y] = Salle(False,x,y)
+
+
     def add_frontier(self, x, y):
         if (x >= 0 and y >= 0 and y < len(self.__grid)
                 and x < len(self.__grid[y]) and self.__grid[y][x] == 0):
@@ -92,11 +108,30 @@ class Map:
     def is_empty(self, cell):
         return cell == 0 or cell == self.FRONTIER
 
-    def display_maze(self):
-        print()
-        print(' ' + '_' * (len(self.__grid[0]) * 2 - 1))
+    def creationFrontiereSalle(self):
         for y, row in enumerate(self.__grid):
-            line = '|'
+            for x, cell in enumerate(row):
+                if (cell & self.E):
+                    self.__salles[x][y].setSalleDoite(self.__salles[x+1][y])
+                    #print("on peut aller a doite de : "+str(x) + " " + str(y))
+                if (cell & self.N):
+                    self.__salles[x][y].setSalleHaut(self.__salles[x][y - 1])
+                    #print("on peut aller en haut de : " + str(x) + " " + str(y))
+                if (cell & self.S):
+                    self.__salles[x][y].setSalleBas(self.__salles[x][y + 1])
+                    #print("on peut aller en bas de : " + str(x) + " " + str(y))
+                if (cell & self.W):
+                    self.__salles[x][y].setSalleGauche(self.__salles[x-1][y])
+                    #print("on peut aller a gauche de : " + str(x) + " " + str(y))
+
+    def display_maze(self):
+        sys.stdout.write("  ")
+        for i in range(self.__tailleX):
+            sys.stdout.write(" "+str(i))
+        print()
+        print('   ' + '_' * (len(self.__grid[0]) * 2 - 1))
+        for y, row in enumerate(self.__grid):
+            line = str(y)+" |"
             for x, cell in enumerate(row):
                 # Dessin du mur ou du passage Sud
                 if self.is_empty(cell) and y + 1 < len(self.__grid) and self.is_empty(self.__grid[y + 1][x]):
@@ -123,7 +158,52 @@ class Map:
         return 0
 
     def seDeplacer(self):
-        print("je me déplace")
+        print("Vous etes en "+str(self.__positionX)+":"+str(self.__positionY))
+        print("Vous pouvez vous déplacer vers :")
+        haut = 0
+        bas =0
+        droite = 0
+        gauche = 0
+        if (self.__salles[self.__positionX][self.__positionY].getSalleHaut()!= None):
+            haut=1
+            print("le haut (h)")
+        if (self.__salles[self.__positionX][self.__positionY].getSalleDroite()!= None):
+            droite=1
+            print("la droite (d)")
+        if (self.__salles[self.__positionX][self.__positionY].getSalleGauche()!= None):
+            gauche=1
+            print("la gauche (g)")
+        if (self.__salles[self.__positionX][self.__positionY].getSalleBas()!= None):
+            bas=1
+            print("le bas (b)")
+        ok = False
+
+        while(ok == False):
+            print("Ou voulez-vous aller?")
+            saisie = input()
+            while(saisie not in ('b','h','g','d')):
+                print("Mauvaise saisie")
+                print("Ou voulez-vous aller?")
+                saisie = input()
+            if((saisie == 'h') and haut):
+                ok = True
+                self.__positionY-=1
+            elif ((saisie == 'b') and bas):
+                ok = True
+                self.__positionY += 1
+            elif ((saisie == 'g') and gauche):
+                ok = True
+                self.__positionX -= 1
+            elif ((saisie == 'd') and droite):
+                ok = True
+                self.__positionX += 1
+            else :
+                print("Impossible d'aller par là...")
+        print("Vous vous déplacer")
+
 
 m = Map(10,10)
 m.genererMap()
+while(1):
+    m.display_maze()
+    m.seDeplacer()
--- Code python/Salle.py
+++ Code python/Salle.py
@@ -3,6 +3,10 @@ class Salle:
         self.__isExplore = isExplore
         self.__x = x
         self.__y = y
+        self.__salleDroite = None
+        self.__salleGauche = None
+        self.__salleHaut = None
+        self.__salleBas = None
 
     def getX(self):
         return self.__x
@@ -15,3 +19,27 @@ class Salle:
 
     def setExplore(self, isExplore):
         self.__isExplore = isExplore
+
+    def setSalleDoite(self, salle):
+        self.__salleDroite = salle
+
+    def setSalleGauche(self, salle):
+        self.__salleGauche = salle
+
+    def setSalleBas(self, salle):
+        self.__salleBas = salle
+
+    def setSalleHaut(self, salle):
+        self.__salleHaut = salle
+
+    def getSalleGauche(self):
+        return self.__salleGauche
+
+    def getSalleDroite(self):
+        return self.__salleDroite
+
+    def getSalleBas(self):
+        return self.__salleBas
+
+    def getSalleHaut(self):
+        return self.__salleHaut
