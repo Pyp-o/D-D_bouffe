511aaccecb4dac0c09734a6304afbf3aeb7bff1f
Clement-Dufaix <clement413@gmail.com>
2019-04-26 10:49:44 +0200

    modif status

--- Code python/Endormi.py
+++ Code python/Endormi.py
@@ -4,5 +4,11 @@ class Endormi(Statut):
     def __init__(self,tourRestant):
         Statut.__init__(self,tourRestant)
 
-    def activerStatut(self):
+    def activerStatut(self, combattant):
         self.setTourRestant(self.getTourRestant()-1)
+
+    def retirerStatut(self, combattant):
+        raise NotImplementedError
+
+    def retirerStatut(self, combattant):
+        combattant.retirerStatut(self)
--- Code python/Paralyse.py
+++ Code python/Paralyse.py
@@ -1,8 +1,16 @@
 from Statut import *
+from random import *
 
 class Paralyse(Statut):
     def __init__(self,tourRestant):
         Statut.__init__(self,tourRestant)
 
-    def activerStatut(self):
+    def activerStatut(self, combattant):
         self.setTourRestant(self.getTourRestant()-1)
+        n = randint(1,10)
+        if (n<4):
+			combattant.setTourFini(True)
+			print("La paralysie l'empeche d'attaquer...")
+       
+    def retirerStatut(self, combattant):
+        combattant.retirerStatut(self)
