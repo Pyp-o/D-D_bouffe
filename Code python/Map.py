7408ccda1ec91b30dbd8ff58b68c3aaad0771fa4
Clement-Dufaix <clement413@gmail.com>
2019-04-29 10:29:14 +0200

    add getch in Map

--- Code python/Map.py
+++ Code python/Map.py
@@ -211,4 +211,13 @@ class Map:
                 print("Impossible d'aller par là...")
         print("Vous vous déplacer")
 
-
+	def getch():
+		fd = sys.stdin.fileno()
+		old_settings = termios.tcgetattr(fd)
+		try:
+			tty.setraw(sys.stdin.fileno())
+			ch = sys.stdin.read(1)
+	 
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+		return ch
