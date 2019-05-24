b41904221bee1807c1af03c6ce1ab6e620ac3ee4
pierre <pierre.henrpi@gmail.com>
2019-05-24 15:14:31 +0200

    modif minimes affichage stat perso

--- Code python/Personnage.py
+++ Code python/Personnage.py
@@ -83,8 +83,8 @@ class Personnage :
         print("\nLe personnage s'appelle :", self.getNom())
 
     def AfficherStat(self):
-        print("-", self.getNom())
-        print('\n**************** Caractéristiques ****************')
+        print("\n-", self.getNom())
+        print('**************** Caractéristiques ****************')
         print("Le personnage ", self.getNom(), " a : ", self.getPV(),"/", self.getPVmax(),"PV", end=" ")
         print(self.getPC(),"/", self.getPCmax(),"PC ", "Attaque :", self.getAttaque(), "Defense :", self.getDefense(), end=" ")
         print("Agilité :", self.getAgilite(), "Initiative :", self.getInitiative())
