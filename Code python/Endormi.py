3c50fdfdfabb7b13473af1bcdd2cbd54f7702880
pierre <pierre.henrpi@gmail.com>
2019-05-27 09:14:47 +0200

    Merge branch 'master' of https://github.com/Pypodhau/D-D_bouffe
    # Conflicts:
    #	Code python/debug.txt

--- Code python/Endormi.py
+++ Code python/Endormi.py
@@ -5,7 +5,7 @@ class Endormi(Statut):
         Statut.__init__(self,nom, tourRestant)
 
     def activerStatut(self, combattant):
-		print(combattant.getNom()+ " est entierement endormi et ne pourra pas jouer ce tour...")
+        print(combattant.getNom()+ " est entierement endormi et ne pourra pas jouer ce tour...")
         self.setTourRestant(self.getTourRestant()-1)
         if (self.getTourRestant()==0):
             self.retirerStatut(combattant)
